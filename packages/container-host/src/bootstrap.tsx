b2221aae1efff267924d91c3f5e0a42caaede794
eviterif <eloy.viteri@skookum.com>
2022-02-15 21:56:06 -0500

    fixed container

--- packages/container-host/src/bootstrap.tsx
+++ packages/container-host/src/bootstrap.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+
+const history = createBrowserHistory()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />,
-  </React.StrictMode>,
+  <Router history={history}>
+    <App />
+  </Router>,
   document.getElementById('root')
 )
 
